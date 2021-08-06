6fa6cc7e0c48dfd6d1e42f335e985899247e1dbf
taxioo <taxioo@naver.com>
2021-08-06 17:56:37 +0900

    지역정보 완성

--- country/views.py
+++ country/views.py
@@ -4,7 +4,16 @@ from django.views.generic import TemplateView
 class globalView(TemplateView):
     template_name = 'country/area.html'
 
-class GlobalListView(TemplateView):
-    template_name = 'country/area_list.html'
+class FranceListView(TemplateView):
+    template_name = 'country/france_list.html'
+
+class SpainListView(TemplateView):
+    template_name = 'country/spain_list.html'
+
+class ItalyListView(TemplateView):
+    template_name = 'country/italy_list.html'
+
+class PortugalListView(TemplateView):
+    template_name = 'country/portugal_list.html'
 
 # Create your views here.
